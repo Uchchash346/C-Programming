2a471dbd355061ae8e1685f17eace5d572c17102
Uchchash346 <mru416@gmail.com>
2024-01-20 01:40:30 +0600

    1.1_Find Data Type Size

---
+++ 1.1_Find Data Type Size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    printf("Integer takes %d byte \n", sizeof(int));
+    printf("Character takes %d byte \n", sizeof(char));
+    printf("Float takes %d byte \n", sizeof(float));
+    printf("Double takes %d byte \n", sizeof(double));
+
+    return 0;
+}
